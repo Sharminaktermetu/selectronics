2f19c9388a14ae64f36fc3c2a831550544e2708e
Sharminaktermetu <sharminacc1@gmail.com>
2023-11-12 23:12:01 +0600

    image

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,10 +1,10 @@
-// const fs = require("fs");
-// const path = require("path");
-// const { upload, s3 } = require("../multer/multer");
+const fs = require("fs");
+const path = require("path");
+const { upload, s3 } = require("../multer/multer");
 
-// const Image = require("../schemas/imageScema");
-// const uploadFile = (req, res) => {
-//   res.send("Received");
-// };
+const Image = require("../schemas/imageScema");
+const uploadFile = (req, res) => {
+  res.send("Received");
+};
 
-// module.exports = { uploadFile };
+module.exports = { uploadFile };
