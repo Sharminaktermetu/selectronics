b9fc11133e4a014d180741fea865efaf58bf33af
Sharminaktermetu <sharminacc1@gmail.com>
2023-12-08 17:46:48 +0600

    new push

--- config/db.js
+++ config/db.js
@@ -5,8 +5,8 @@ const connectDB = async () => {
   mongoose
     .connect(`mongodb+srv://qawmi:gUweWqnFYVX4BX1P@qawmi-university.ude8vl9.mongodb.net/?retryWrites=true&w=majority`,
       {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
       //  bufferTimeoutMS: 30000,
       // socketTimeoutMS: 1000 // 30 seconds
       // bufferCommands: false, // Disables buffering
