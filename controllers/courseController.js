caa4eb16e770c0ae17df2ce169cc23b75402067a
Sharminaktermetu <sharminacc1@gmail.com>
2023-12-14 16:45:09 +0600

    change

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -137,9 +137,9 @@ const courseUpdate = asyncHandler(async (req, res) => {
 
 const getSingleCourse = asyncHandler(async (req, res) => {
   try {
-    const title = req.params.title;
-    console.log(title);
-    const course = await Course.findOne({ title}).select({
+    const id = req.params.id;
+    // console.log(title);
+    const course = await Course.findOne({ _id: ObjectId(id) }).select({
       'curriculum.lessons.quizes': 0,
       'curriculum.lessons.video': 0,
       'curriculum.lessons.note': 0,
