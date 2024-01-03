cd839b20316aeb5299a1232ec5b96d2d948780a8
Sharminaktermetu <sharminacc1@gmail.com>
2024-01-03 20:43:11 +0600

    email design

--- controllers/otpController.js
+++ controllers/otpController.js
@@ -90,7 +90,9 @@ const verifyOtp = async (req, res) => {
 }
 
 const forgotSendOtp = async (req, res) => {
-
+    const user = await User.findOne({
+        number: req.body.number
+    });
     const OTP = otpGenerator.generate(4, {
         digits: true, alphabets: false, upperCaseAlphabets: false, specialChars: false,lowerCaseAlphabets:false,
     });
@@ -117,37 +119,38 @@ const forgotSendOtp = async (req, res) => {
     }
     return res.json({ success: true, message: 'OTP sent successfully' });
 }
-const verifyForgotOtp = async (req, res) => {
-    const otpHolder = await Otp.find({
-        number: req.body.number
-    });
-    if (otpHolder.length === 0) return res.status(400).send("You use an Expired OTP!");
-    const rightOtpFind = otpHolder[otpHolder.length - 1];
-    const validUser = await bcrypt.compare(req.body.otp, rightOtpFind.otp);
+// const verifyForgotOtp = async (req, res) => {
+//     const otpHolder = await Otp.find({
+//         number: req.body.number
+//     });
+//     if (otpHolder.length === 0) return res.status(400).send("You use an Expired OTP!");
+//     const rightOtpFind = otpHolder[otpHolder.length - 1];
+//     const validUser = await bcrypt.compare(req.body.otp, rightOtpFind.otp);
 
-    if (rightOtpFind.number === req.body.number && validUser) {
-        const user = new User(_.pick(req.body, ["number"]));
-        const token = user.generateJWT();
-        // const result = await user.save();
-        const OTPDelete = await Otp.deleteMany({
-            number: rightOtpFind.number
-        });
+//     if (rightOtpFind.number === req.body.number && validUser) {
+//         const user = new User(_.pick(req.body, ["number"]));
+//         const token = user.generateJWT();
+//         // const result = await user.save();
+//         const OTPDelete = await Otp.deleteMany({
+//             number: rightOtpFind.number
+//         });
 
-        return res.status(200).send({
-            success: true,
-            message: "OTP Verify Successfull.!!",
-            // token: {token},
-            // data: userNumber
-        });
-    } else {
-        return res.status(400).send("Your OTP was wrong!")
-    }
-}
+//         return res.status(200).send({
+//             success: true,
+//             message: "OTP Verify Successfull.!!",
+//             // token: {token},
+//             // data: userNumber
+//         });
+//     } else {
+//         return res.status(400).send("Your OTP was wrong!")
+//     }
+// }
 
 
 module.exports = {
     sendOtp,
     verifyOtp,
-    forgotSendOtp,verifyForgotOtp
+    forgotSendOtp,
+    // verifyForgotOtp
 
 };
--- routeHandler/otpHandler.js
+++ routeHandler/otpHandler.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {
-    sendOtp,verifyOtp,forgotSendOtp,verifyForgotOtp
+    sendOtp,verifyOtp,forgotSendOtp,
+    // verifyForgotOtp
 
 } = require('../controllers/otpController');
 
@@ -9,7 +10,7 @@ const router = express.Router();
 router.route('/send-otp').post(sendOtp);
 router.route('/verify-otp').post(verifyOtp);
 router.route('/forgot-otp').post(forgotSendOtp);
-router.route('/forgot-verify-otp').post(verifyForgotOtp);
+// router.route('/forgot-verify-otp').post(verifyForgotOtp);
 
 
 module.exports = router;
