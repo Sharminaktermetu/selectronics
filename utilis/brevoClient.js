edd2ccf65b603ded7d2b6328efd80ea267f0a2ec
Sharminaktermetu <sharminacc1@gmail.com>
2026-01-09 21:27:04 +0600

    re-add brevoClient without any secrets

--- utilis/brevoClient.js
+++ utilis/brevoClient.js
@@ -1,11 +1,10 @@
-import React from 'react'
 
-const brevoClient = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const SibApiV3Sdk = require('sib-api-v3-sdk');
 
-export default brevoClient
+let defaultClient = SibApiV3Sdk.ApiClient.instance;
+let apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = "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"; // store key in .env
+
+const brevoApi = new SibApiV3Sdk.TransactionalEmailsApi();
+
+module.exports = brevoApi;
